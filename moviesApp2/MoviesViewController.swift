bc164004a513d531ee531201bfc52eba36fc8f99
Otávio Fraga da Silva <otavinhofs1@gmail.com>
2022-12-02 13:25:54 -0300

    Initial Commit

---
+++ moviesApp2/MoviesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moviesApp2
+//
+//  Created by Otávio da Silva on 02/12/22.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
